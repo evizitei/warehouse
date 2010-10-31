639f4c5703b29de9ab36e8da8fb0f90fb998c7ca
evizitei <ethan.vizitei@gmail.com>
2010-10-31 13:21:51 -0500

    initial commit
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.1'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem "haml"
+gem "slim"
+gem "heroku"
+
+gem 'pg',:group=>:production
+
+group :development, :test do
+  gem 'webrat'
+  gem 'jasmine'
+  gem "rspec-rails"
+  gem "cucumber-rails",     "0.3.2"
+  gem 'database_cleaner'
+  gem 'cucumber'
+  gem 'spork'
+  gem 'launchy'
+  gem "factory_girl_rails", "1.0"
+  gem "capybara",         "0.3.9"
+  gem "timecop",          "0.3.5"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FireWarehouse::Application.config.secret_token = 'fc85c37219f81e911821cd4628617a4990bb831d3b4f5fa273a0fb21e669caacd20f729e7917f7700801865198712d8074852a882db5a7ba0eea42dcc3943b8c'
